5c231268078651060b74544fcde7f4c72fc74e36
Abhishek-cmd13 <75055349+Abhishek-cmd13@users.noreply.github.com>
2023-07-26 13:43:50 +0530

    Some Issue got sorted

--- app/main.py
+++ app/main.py
@@ -8,9 +8,21 @@ app = FastAPI()
 
 class TickerSymbols(BaseModel):
     symbols: list
+
 class PortfolioInput(BaseModel):
     stocks: list
     new_stock: str
+
+def calculate_mean_correlation_excluding(correlation_matrix, new_stock):
+    correlation_matrix_excluding = correlation_matrix.drop(columns=new_stock, index=new_stock)
+    return np.mean(correlation_matrix_excluding.values)
+
+def calculate_mean_correlation_including(correlation_matrix, new_stock):
+    correlation_matrix_including = correlation_matrix.drop(columns=new_stock, index=new_stock)
+    new_stock_correlation = correlation_matrix[new_stock].drop(index=new_stock)
+    correlation_matrix_including[new_stock] = new_stock_correlation
+    correlation_matrix_including[new_stock][new_stock] = 1.0  # Set correlation with itself to 1.0
+    return np.mean(correlation_matrix_including.values)
 
 @app.post("/diversification")
 def assess_diversification(portfolio_input: PortfolioInput):
@@ -37,13 +49,9 @@ def assess_diversification(portfolio_input: PortfolioInput):
         df = pd.DataFrame(data)
         correlation_matrix = df.corr()
         
-        # Calculate mean correlation excluding the new stock
         mean_correlation_excluding_new_stock = calculate_mean_correlation_excluding(correlation_matrix, new_stock)
-        
-        # Calculate mean correlation including the new stock
         mean_correlation_including_new_stock = calculate_mean_correlation_including(correlation_matrix, new_stock)
         
-        # Determine if the addition of the new stock is diversifying the portfolio
         if mean_correlation_including_new_stock < mean_correlation_excluding_new_stock:
             diversification_result = "Adding the new stock is diversifying the portfolio."
         else:
@@ -51,27 +59,13 @@ def assess_diversification(portfolio_input: PortfolioInput):
         
         return {
             "correlation_matrix": correlation_matrix.to_dict(),
-            "mean_correlation_excluding_new_stock": mean_correlation_excluding_new_stock,
-            "mean_correlation_including_new_stock": mean_correlation_including_new_stock,
+            "mean_correlation_excluding_new_stock": round(mean_correlation_excluding_new_stock, 2),
+            "mean_correlation_including_new_stock": round(mean_correlation_including_new_stock, 2),
             "diversification_result": diversification_result
         }
     
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-def calculate_mean_correlation_excluding(correlation_matrix, new_stock):
-    # Exclude the new stock from the correlation matrix
-    correlation_matrix_excluding = correlation_matrix.drop(columns=new_stock, index=new_stock)
-    return np.mean(correlation_matrix_excluding.values)
-
-def calculate_mean_correlation_including(correlation_matrix, new_stock):
-    # Include the new stock in the correlation matrix
-    correlation_matrix_including = correlation_matrix.drop(columns=new_stock, index=new_stock)
-    new_stock_correlation = correlation_matrix[new_stock].drop(index=new_stock)
-    correlation_matrix_including[new_stock] = new_stock_correlation
-    correlation_matrix_including[new_stock][new_stock] = 1.0  # Set correlation with itself to 1.0
-    return np.mean(correlation_matrix_including.values)
-
 
 @app.post("/correlation")
 def calculate_correlation(ticker_symbols: TickerSymbols):
